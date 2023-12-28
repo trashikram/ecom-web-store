a2ec023b141132278c5ae71240a369f3c48b899f
trashikram <118533822+trashikram@users.noreply.github.com>
2023-12-28 08:58:46 +0600

    448
    6646

---
+++ Start.java
@@ -0,0 +1,14 @@
+import Project.*;
+
+import java.lang.*;
+
+public class Start
+{
+	public static void main(String args[])
+	{
+		
+		Homepage hp = new Homepage();
+		hp.setVisible(true);
+		
+}
+}
